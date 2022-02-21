7087348fc94a74fdf0f6ee2f46201bfc4f756103
Surajxxx <suraj.dubey191@gmail.com>
2022-02-21 16:58:29 +0530

    My first try

---
+++ src/routes/logger.js
@@ -0,0 +1,8 @@
+let url = "https://www.google.com"
+
+function myText (message){
+    console.log(message)
+}
+
+module.exports.googleLink = url
+module.exports.myPrintText = myText
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,10 @@
+let obj = require('./logger.js')
 const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    obj.myPrintText("hello i am thorium")
+    console.log(obj.googleLink)
     res.send('My first ever api!')
 });
 
