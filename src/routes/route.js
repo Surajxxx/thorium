2a5f00a9312bc7e012f3a59d0916f9d872b71136
Surajxxx <suraj.dubey191@gmail.com>
2022-02-22 15:44:15 +0530

    assignment for get-api  on 22-02-2022

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,8 +6,67 @@ router.get('/students/:name', function(req, res) {
     console.log(studentName)
     res.send(studentName)
 })
+router.get('/movies', function(req, res){
+    let movies = ["James-bond", "Dil", "Dhoom", "Sin-city", "Black-Friday"]
+    res.send(movies)
+})
+router.get('/movies/:number', function(req, res){
+    let movies = ["James-bond", "Dil", "Dhoom", "Sin-city", "Black-Friday"];
+    let index = req.params.number
+    if(index >= movies.length ){
+        res.send("Invalid input")
+    }else{
+    res.send(movies[index])
+    }
+})
+router.get('/films', function(req, res){
+    let films = [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Demo"
+       }];
+       res.send(films)
+       
+})
+router.get ('/films/:filmId', function(req, res){
+    let movieId  = req.params.filmId
+    let films =[ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Demo"
+       }];
 
 
+           movieId <= 3 ? res.send(films[movieId]) : res.send("invalid input")
+        // for(let i=0; i< films.length; i++){
+        //     if (movieId >= 5){
+        //         res.send("invalid film ID")
+        //     }else if (films[i].id !== movieId) {
+        //                 continue;
+        //     }else {
+        //         res.send(films[i].name)
+        //     }
+        // } 
+
+
+       
+})
 
 
 module.exports = router;
